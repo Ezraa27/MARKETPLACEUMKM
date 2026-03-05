c46c95616810581f617ce9dcb4e9f46b9539f459
Ezraa27 <ezrameritaringoringo@gmail.com>
2026-03-05 19:32:49 +0700

    Update

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<div style="text-align:center; padding:20px;">
+    <h1>Hubungi Kami</h1>
+
+    <p>
+        Silakan menghubungi admin kami jika Anda mengalami masalah
+        atau memiliki pertanyaan mengenai website kami.
+    </p>
+
+    <p>Email : admin@umkm.com</p>
+    <p>WhatsApp : 0812-3456-7890</p>
+    <p>Instagram : umkmterpercaya_</p>
+</div>
