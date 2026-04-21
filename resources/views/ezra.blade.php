b4aaf602e9266256c62ece0f4b997bf3d9135848
Ezraa27 <ezrameritaringoringo@gmail.com>
2026-04-21 11:56:31 +0700

    menambahkan konfigurasi tailwind  CSS dan Flowbite, serta membuat file css dan blade untuk styling halaman

--- resources/views/ezra.blade.php
+++ resources/views/ezra.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="{{ asset('styles/style_ezra.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -11,6 +12,9 @@
 
     <img src="{{ asset('images/ezra1.png') }}" alt="ezra1">
     <img src="{{ asset('images/ezra2.png') }}" alt="ezra2">
+    <div class="bg-blue-500 p-4 m-4 rounded-lg text-white">
+        Ini pakai Tailwind
+    </div>
 </body>
 
 </html>
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 });
